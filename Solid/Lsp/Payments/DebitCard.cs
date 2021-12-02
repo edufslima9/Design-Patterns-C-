8fb44389998a7888bda8fb635216e8483165395f
Eduardo Felipe <eduardo.lima@cmtech.com.br>
2021-12-02 20:41:51 -0300

    Liskov Substitution Principle - LSP

---
+++ Solid/Lsp/Payments/DebitCard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lsp.Payments
+{
+    class DebitCard : NubankCard
+    {
+        public override void Validate()
+        {
+            Console.WriteLine("Validando o saldo da conta...");
+            Console.WriteLine("Saldo disponível!");
+        }
+    }
+}
---
+++ Solid/Lsp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Lsp.Payments;
+using System;
+
+namespace Lsp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NubankRewards card = new NubankRewards();
+            card.Validate();
+            card.CollectPayment();
+
+            Console.ReadLine();
+        }
+    }
+}
