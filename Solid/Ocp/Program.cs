9df8750aa805db0825f6d05ec540a5f7128a9da2
Eduardo Felipe <eduardo.lima@cmtech.com.br>
2021-12-02 20:27:52 -0300

    Open/Closed Principle - OCP

---
+++ Solid/Ocp/Program.cs
@@ -0,0 +1,28 @@
+﻿using Ocp.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TypeVehicle type = TypeVehicle.CAR;
+
+            if(type == TypeVehicle.CAR)
+            {
+                Car vechicle = new Car("Azul", 2022, 2.0, 5, 4);
+            }
+            else
+            {
+                Motorcycle vechicle = new Motorcycle("Branco", 2023, 250);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
