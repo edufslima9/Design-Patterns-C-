97fef01523abaf91c47239a2b7c7802b0b5a2d46
Eduardo Felipe <eduardo.lima@cmtech.com.br>
2021-12-02 19:46:20 -0300

    SRP comments

--- Solid/Srp/Program.cs
+++ Solid/Srp/Program.cs
@@ -6,6 +6,10 @@ namespace Srp
     {
         static void Main(string[] args)
         {
+            // [S] - Single Responsibility Principle
+            // Princípio da Responsabilidade Única
+
+            // "Nunca deve have rmais de um motivo para uma classe mudar"
         }
     }
 }
