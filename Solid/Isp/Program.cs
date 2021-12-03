31db310d2ef3a13d909afca2bc79e7cf1024abd7
Eduardo Felipe <eduardo.lima@cmtech.com.br>
2021-12-02 21:05:38 -0300

    Interface Segregation Principle - ISP

---
+++ Solid/Isp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Isp.Vehicles;
+using System;
+
+namespace Isp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Preto", 2021, 4.0, 2, 2);
+            Motorcycle motorcycle = new Motorcycle("Vermelho", 2021, 600);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Solid/Isp/Vehicles/Motorcycle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isp.Vehicles
+{
+    class Motorcycle : IVehicleMotorcycle
+    {
+        private string color;
+        private int year;
+        private double engine;
+
+        public Motorcycle(string color, int year, double engine)
+        {
+            ConfigureMotorcycle(color, year, engine);
+        }
+
+        public void ConfigureMotorcycle(string color, int year, double engine)
+        {
+            this.color = color;
+            this.year = year;
+            this.engine = engine;
+
+            Console.WriteLine($"Criando uma moto ano {year}, {engine} cilindradas, da cor {color}.");
+            StartVehicle();
+        }
+
+        public void StartVehicle()
+        {
+            Console.WriteLine("Ligando os motores.");
+        }
+    }
+}
