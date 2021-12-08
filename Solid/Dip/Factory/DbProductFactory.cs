80b4d2ad418c44a31e8b786d0444f30fe3dbd362
Eduardo Felipe <eduardo.lima@cmtech.com.br>
2021-12-08 00:26:21 -0300

    Dependency Inversion Principle - DIP

---
+++ Solid/Dip/Factory/DbProductFactory.cs
@@ -0,0 +1,27 @@
+﻿using Dip.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dip.Factory
+{
+    class DbProductFactory
+    {
+
+        public static DbProduct Create()
+        {
+            if (ConfigurationManager.AppSettings["DB"] == "SQLSERVER")
+            {
+                return new SQLServerProduct();
+            }
+            else
+            {
+                return new MongoDBProduct();
+            }
+        }
+
+    }
+}
---
+++ Solid/Dip/Program.cs
@@ -0,0 +1,20 @@
+﻿using Dip.Payments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentProccess payment = new PaymentProccess();
+            payment.Pay("ABC123");
+
+            Console.ReadLine();
+        }
+    }
+}
